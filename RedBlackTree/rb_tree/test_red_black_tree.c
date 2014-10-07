4c9dab37fb01f8a8c7dd607c9a48ef38565955a5
prasannabe2004 <prasannabe2004@gmail.com>
2014-10-06 19:03:01 -0500

    Alignment fixed.

--- RedBlackTree/rb_tree/test_red_black_tree.c
+++ RedBlackTree/rb_tree/test_red_black_tree.c
@@ -10,8 +10,6 @@ void IntDest(void* a)
     free((int*)a);
 }
 
-
-
 int IntComp(const void* a,const void* b) 
 {
     if( *(int*)a > *(int*)b) return(1);
@@ -56,89 +54,106 @@ int main()
         {
             case 1:
             {
-                    printf("type key for new node\n");
-                    scanf("%i",&newKey);
-                    newInt=(int*) malloc(sizeof(int));
-                    *newInt=newKey;
-                    RBTreeInsert(tree,newInt,0);
-                }
-                break;
+                printf("type key for new node\n");
+                scanf("%i",&newKey);
+                newInt=(int*) malloc(sizeof(int));
+                *newInt=newKey;
+                RBTreeInsert(tree,newInt,0);
+            }
+            break;
 
             case 2:
-                {
-                    printf("type key of node to remove\n");
-                    scanf("%i",&newKey);
-                    if ( ( newNode=RBExactQuery(tree,&newKey ) ) ) RBDelete(tree,newNode);/*assignment*/
-                    else printf("key not found in tree, no action taken\n");
-                }
-                break;
+            {
+                printf("type key of node to remove\n");
+                scanf("%i",&newKey);
+                if ( ( newNode=RBExactQuery(tree,&newKey ) ) ) 
+                    RBDelete(tree,newNode);/*assignment*/
+                else
+                    printf("key not found in tree, no action taken\n");
+            }
+            break;
 
             case 3:
+            {
+                printf("type key of node to query for\n");
+                scanf("%i",&newKey);
+                if ( ( newNode = RBExactQuery(tree,&newKey) ) ) 
+                {/*assignment*/
+                    printf("data found in tree at location %i\n",(int)newNode);
+                } 
+                else 
                 {
-                    printf("type key of node to query for\n");
-                    scanf("%i",&newKey);
-                    if ( ( newNode = RBExactQuery(tree,&newKey) ) ) {/*assignment*/
-                        printf("data found in tree at location %i\n",(int)newNode);
-                    } else {
-                        printf("data not in tree\n");
-                    }
+                    printf("data not in tree\n");
                 }
-                break;
+            }
+            break;
             case 4:
-                {
-                    printf("type key of node to find predecessor of\n");
-                    scanf("%i",&newKey);
-                    if ( ( newNode = RBExactQuery(tree,&newKey) ) ) {/*assignment*/
-                        newNode=TreePredecessor(tree,newNode);
-                        if(tree->nil == newNode) {
-                            printf("there is no predecessor for that node (it is a minimum)\n");
-                        } else {
-                            printf("predecessor has key %i\n",*(int*)newNode->key);
-                        }
-                    } else {
-                        printf("data not in tree\n");
+            {
+                printf("type key of node to find predecessor of\n");
+                scanf("%i",&newKey);
+                if ( ( newNode = RBExactQuery(tree,&newKey) ) ) 
+                {/*assignment*/
+                    newNode=TreePredecessor(tree,newNode);
+                    if(tree->nil == newNode) 
+                    {
+                        printf("there is no predecessor for that node (it is a minimum)\n");
+                    } else 
+                    {
+                        printf("predecessor has key %i\n",*(int*)newNode->key);
                     }
                 }
-                break;
+                else
+                {
+                    printf("data not in tree\n");
+                }
+            }
+            break;
             case 5:
+            {
+                printf("type key of node to find successor of\n");
+                scanf("%i",&newKey);
+                if ( (newNode = RBExactQuery(tree,&newKey) ) ) 
                 {
-                    printf("type key of node to find successor of\n");
-                    scanf("%i",&newKey);
-                    if ( (newNode = RBExactQuery(tree,&newKey) ) ) {
-                        newNode=TreeSuccessor(tree,newNode);
-                        if(tree->nil == newNode) {
-                            printf("there is no successor for that node (it is a maximum)\n");
-                        } else {
-                            printf("successor has key %i\n",*(int*)newNode->key);
-                        }
-                    } else {
-                        printf("data not in tree\n");
+                    newNode=TreeSuccessor(tree,newNode);
+                    if(tree->nil == newNode) 
+                    {
+                        printf("there is no successor for that node (it is a maximum)\n");
+                    }
+                    else
+                    {
+                        printf("successor has key %i\n",*(int*)newNode->key);
                     }
                 }
-                break;
+                else
+                {
+                    printf("data not in tree\n");
+                }
+            }
+            break;
             case 6:
+            {
+                printf("type low and high keys to see all keys between them\n");
+                scanf("%i %i",&newKey,&newKey2);
+                enumResult=RBEnumerate(tree,&newKey,&newKey2);	  
+                while ( (newNode = StackPop(enumResult)) ) 
                 {
-                    printf("type low and high keys to see all keys between them\n");
-                    scanf("%i %i",&newKey,&newKey2);
-                    enumResult=RBEnumerate(tree,&newKey,&newKey2);	  
-                    while ( (newNode = StackPop(enumResult)) ) {
-                        tree->PrintKey(newNode->key);
-                        printf("\n");
-                    }
-                    free(enumResult);
+                    tree->PrintKey(newNode->key);
+                    printf("\n");
                 }
-                break;
+                free(enumResult);
+            }
+            break;
             case 7:
-                {
-                    RBTreePrint(tree);
-                }
-                break;
+            {
+                RBTreePrint(tree);
+            }
+            break;
             case 8:
-                {
-                    RBTreeDestroy(tree);
-                    return 0;
-                }
-                break;
+            {
+                RBTreeDestroy(tree);
+                return 0;
+            }
+            break;
             default:
                 printf("Invalid input; Please try again.\n");
         }
