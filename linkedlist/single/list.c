6abbf48103b6c89680808c25ef2df29cb72cc99f
prasannabe2004 <prasannabe2004@gmail.com>
2016-01-28 15:09:06 -0600

    Single list implementation and linear search

--- linkedlist/single/list.c
+++ linkedlist/single/list.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
+#include <time.h>
 
 struct test_struct
 {
@@ -11,13 +12,15 @@ struct test_struct
 struct test_struct *head = NULL;
 struct test_struct *curr = NULL;
 
+struct timeval  tv1, tv2;
+
 struct test_struct* create_list(int val)
 {
-    printf("\n creating list with headnode as [%d]\n",val);
+    printf("Creating list with headnode as [%d]\n",val);
     struct test_struct *ptr = (struct test_struct*)malloc(sizeof(struct test_struct));
     if(NULL == ptr)
     {
-        printf("\n Node creation failed \n");
+        printf("Node creation failed \n");
         return NULL;
     }
     ptr->val = val;
@@ -27,37 +30,25 @@ struct test_struct* create_list(int val)
     return ptr;
 }
 
-struct test_struct* add_to_list(int val, bool add_to_end)
+struct test_struct* add_to_list(int val)
 {
     if(NULL == head)
     {
         return (create_list(val));
     }
 
-    if(add_to_end)
-        printf("\n Adding node to end of list with value [%d]\n",val);
-    else
-        printf("\n Adding node to beginning of list with value [%d]\n",val);
-
     struct test_struct *ptr = (struct test_struct*)malloc(sizeof(struct test_struct));
     if(NULL == ptr)
     {
-        printf("\n Node creation failed \n");
+        printf("Node creation failed \n");
         return NULL;
     }
     ptr->val = val;
     ptr->next = NULL;
 
-    if(add_to_end)
-    {
-        curr->next = ptr;
-        curr = ptr;
-    }
-    else
-    {
-        ptr->next = head;
-        head = ptr;
-    }
+    curr->next = ptr;
+    curr = ptr;
+
     return ptr;
 }
 
@@ -67,13 +58,17 @@ struct test_struct* search_in_list(int val, struct test_struct **prev)
     struct test_struct *tmp = NULL;
     bool found = false;
 
-    printf("\n Searching the list for value [%d] \n",val);
+    printf("Searching the list for value [%d] \n",val);
+	
+	gettimeofday(&tv1, NULL);
 
     while(ptr != NULL)
     {
         if(ptr->val == val)
         {
             found = true;
+			gettimeofday(&tv2, NULL);
+			printf ("Linear Search took %f seconds\n",(double) (tv2.tv_usec - tv1.tv_usec) / 1000000 + (double) (tv2.tv_sec - tv1.tv_sec));
             break;
         }
         else
@@ -100,7 +95,7 @@ int delete_from_list(int val)
     struct test_struct *prev = NULL;
     struct test_struct *del = NULL;
 
-    printf("\n Deleting value [%d] from list\n",val);
+    printf("Deleting value [%d] from list\n",val);
 
     del = search_in_list(val,&prev);
     if(del == NULL)
@@ -132,13 +127,13 @@ void print_list(void)
 {
     struct test_struct *ptr = head;
 
-    printf("\n -------Printing list Start------- \n");
+    printf("-------Printing list Start-------\n");
     while(ptr != NULL)
     {
-        printf("\n [%d] \n",ptr->val);
+        printf("[%d]->",ptr->val);
         ptr = ptr->next;
     }
-    printf("\n -------Printing list End------- \n");
+    printf("\n");
 
     return;
 }
@@ -150,41 +145,19 @@ int main(void)
 
     print_list();
 
-    for(i = 5; i<10; i++)
-        add_to_list(i,true);
+    for(i = 0; i<100000000; i++)
+        add_to_list(i);
+    
+	//print_list();
 
-    print_list();
-
-    for(i = 4; i>0; i--)
-        add_to_list(i,false);
-
-    print_list();
-
-    for(i = 1; i<10; i += 4)
+	ptr = search_in_list(99999999, NULL);
+	if(NULL == ptr)
+	{
+    	printf("Search [val = %d] failed, no such element found\n",i);
+    }
+    else
     {
-        ptr = search_in_list(i, NULL);
-        if(NULL == ptr)
-        {
-            printf("\n Search [val = %d] failed, no such element found\n",i);
-        }
-        else
-        {
-            printf("\n Search passed [val = %d]\n",ptr->val);
-        }
-
-        print_list();
-
-        ret = delete_from_list(i);
-        if(ret != 0)
-        {
-            printf("\n delete [val = %d] failed, no such element found\n",i);
-        }
-        else
-        {
-            printf("\n delete [val = %d]  passed \n",i);
-        }
-
-        print_list();
+        printf("Search passed [val = %d]\n",ptr->val);
     }
 
     return 0;
